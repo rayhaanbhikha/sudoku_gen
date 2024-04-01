5d644bc5bd8b4e37222f0b9f38a23d66b80b7664
Rayhaan Bhikha <Rayhaanbhikha3@hotmail.com>
2024-04-01 18:33:56 +0100

    initial commit.

---
+++ src/main/kotlin/sudoku_gen/Grid.kt
@@ -0,0 +1,243 @@
+package com.example.sudoku_gen
+
+class GridIterator(private val grid: Grid) : Iterator<Cell> {
+    private var row = 0
+    private var col = 0
+
+    override fun hasNext(): Boolean {
+        return row <= grid.maxRow
+    }
+
+    override fun next(): Cell {
+        val item = grid[Coord(row, col)]
+        when (col) {
+            grid.maxCol -> {
+                col = 0
+                row++
+            }
+
+            else -> col++
+        }
+
+        return item
+    }
+}
+
+
+class Grid : Iterable<Cell> {
+    private val gridValues: MutableMap<Coord, Cell>
+    val maxRow: Int = 8
+    val maxCol: Int = 8
+
+    constructor(values: Map<Coord, Cell>) {
+        this.gridValues = values.toMutableMap()
+    }
+
+    companion object {
+        // extension functions added separately
+    }
+
+    constructor() {
+        gridValues = mutableMapOf()
+        for (rowIndex in 0..maxRow) {
+            for (colIndex in 0..maxCol) {
+                val coord = Coord(rowIndex, colIndex)
+                this.gridValues[coord] = defaultValue(coord)
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Grid) return false
+        return other.gridValues == gridValues
+    }
+
+    override fun iterator(): Iterator<Cell> {
+        return GridIterator(this)
+    }
+
+    operator fun get(coord: Coord) = gridValues[coord] ?: defaultValue(coord)
+
+    operator fun set(coord: Coord, value: Cell) {
+        gridValues[coord] = value
+    }
+
+    fun clearCell(coord: Coord) {
+        gridValues[coord] = defaultValue(coord)
+    }
+
+    fun emptyCells() = gridValues.values.filter { it.isEmpty() }
+
+    fun copy(): Grid {
+        return Grid(gridValues)
+    }
+
+    private fun defaultValue(coord: Coord) = Cell(coord = coord)
+
+    fun isComplete() = this.all { !it.isEmpty() }
+
+    fun getValidValues(coord: Coord): List<Int> {
+        // TODO: must be a better way.
+        val validValues = mutableSetOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        val cell = get(coord)
+
+        linkedCells(cell).forEach {
+            validValues.remove(it.value)
+        }
+
+        return validValues.toList()
+    }
+
+    private fun linkedCells(initialCell: Cell): Iterator<Cell> {
+        return LinkedCellsIterator(this, initialCell)
+    }
+
+    // for testing only.
+    fun clearCells(coords: List<Coord>) {
+        coords.forEach { coord ->
+            clearCell(coord)
+        }
+    }
+}
+
+fun Grid.legal(coord: Coord, value: Int): Boolean {
+    for (index in 0..8) {
+        val rowCoord = coord.copy(row = index)
+        val colCoord = coord.copy(col = index)
+        if (this[rowCoord].value == value || this[colCoord].value == value) {
+            return false
+        }
+    }
+
+    // Check 3x3 box
+    val boxRowStart = coord.row - (coord.row % 3)
+    val boxColStart = coord.col - (coord.col % 3)
+    for (row in boxRowStart until boxRowStart + 3) {
+        for (col in boxColStart until boxColStart + 3) {
+            val newCoord = coord.copy(row = row, col = col)
+            if (this[newCoord].value == value) return false
+        }
+    }
+
+    return true
+}
+
+fun Grid.nextCoord(currentCoord: Coord): Coord? {
+//    return when {
+//        // reached end of grid.
+//        currentCoord.col == this.maxCol && currentCoord.row == this.maxRow -> null
+//        // reached end of column but not rows.
+//        currentCoord.col == this.maxCol && currentCoord.row < this.maxRow -> Coord(
+//            col = 0,
+//            row = currentCoord.row + 1
+//        )
+//        // next coord in column.
+//        currentCoord.col < this.maxCol -> currentCoord.copy(
+//            col = currentCoord.col + 1,
+//        )
+//        else -> null
+//    }
+    return when {
+        currentCoord.row < this.maxRow && currentCoord.col < this.maxCol -> currentCoord.copy(
+            row = currentCoord.row + 1
+        )
+        currentCoord.row == this.maxRow && currentCoord.col < this.maxCol -> Coord(
+            row = 0,
+            col = currentCoord.col + 1
+        )
+        currentCoord.col == this.maxCol && currentCoord.row < this.maxRow -> currentCoord.copy(
+            row = currentCoord.row + 1
+        )
+        else -> null
+    }
+}
+
+fun Grid.print() {
+    val row = "------+-------+------"
+    val emptyValue = "."
+    val space = " "
+    val divider = " | "
+    println()
+    for (cell in this) {
+        when (cell.value) {
+            0 -> print(emptyValue)
+            else -> print("${cell.value}")
+        }
+        when (cell.coord.col) {
+            maxCol -> {
+                println()
+                when (cell.coord.row) {
+                    2, 5 -> {
+                        println(row)
+                    }
+                }
+            }
+
+            2, 5 -> print(divider)
+            else -> print(space)
+        }
+    }
+    println()
+}
+
+fun Grid.fillRandom(): Boolean {
+    if (this.isComplete()) return true
+
+    for (cell in this) {
+        // if cell is not empty then continue.
+        if (!cell.isEmpty()) continue
+
+        // random value is selected which is valid.
+        for (possibleValue in this.getValidValues(cell.coord).shuffled()) {
+            // set cell value
+            this[cell.coord] = Cell(coord = cell.coord, value = possibleValue)
+            if (this.fillRandom()) {
+                return true
+            } else {
+                this[cell.coord] = Cell(coord = cell.coord, value = 0)
+            }
+        }
+        // no possible values worked need to backtrack.
+        return false
+    }
+    return false
+}
+
+fun Grid.Companion.from(values: List<List<Int>>): Grid {
+    val gridValues = mutableMapOf<Coord, Cell>()
+    values.forEachIndexed { rowIndex, row ->
+        row.forEachIndexed { colIndex, value ->
+            val coord = Coord(rowIndex, colIndex)
+            gridValues[coord] = Cell(coord, value)
+        }
+    }
+    return Grid(gridValues)
+}
+
+fun Grid.Companion.from(rawString: String): Grid {
+    val gridValues = mutableMapOf<Coord, Cell>()
+    var rowIndex = 0
+    var colIndex = 0
+
+    val chars = rawString.split("").drop(1).dropLast(1)
+
+    for (value in chars) {
+        val cellValue = if (value == ".") {
+            0
+        } else {
+            value.toInt()
+        }
+        val cell = Cell(coord = Coord(rowIndex, colIndex), value = cellValue)
+        gridValues[cell.coord] = cell
+
+        if (colIndex == 8) {
+            colIndex = 0
+            rowIndex++
+        } else {
+            colIndex++
+        }
+    }
+
+    return Grid(gridValues)
+}
---
+++ src/main/kotlin/sudoku_gen/SudokuGen.kt
@@ -0,0 +1,79 @@
+package com.example.sudoku_gen
+
+import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
+
+fun main() {
+    var holedGrid: Grid
+
+    val timeTaken = measureTimedValue {
+        val grid = Grid()
+        grid.fillRandom()
+
+        val (_holedGrid, holes) = pokeHoles(grid, 55)
+        holedGrid = _holedGrid
+
+        grid
+    }
+
+    println(timeTaken.duration)
+
+    holedGrid.print()
+}
+
+
+// randomly find elements and poke a hole.
+// then try and solve the puzzle
+// when solving the puzzle there can only be one solution.
+// (consider each 'hole' a possible starting point).
+// if no solution or more than 1 solution is found, consider it a failure.
+// put hole back.
+fun pokeHoles(grid: Grid, holes: Int): Pair<Grid, List<Cell>> {
+    // TODO: Depending on number of holes requested (i.e. holes > 55)
+    // we should start a few coroutines together.
+    val gridWithEmptyCells = grid.copy()
+    val cellsRemoved = mutableListOf<Cell>()
+
+    while(cellsRemoved.size < holes) {
+        val rowIndex = (0..gridWithEmptyCells.maxRow).random()
+        val colIndex = (0..gridWithEmptyCells.maxCol).random()
+        val coord = Coord(rowIndex, colIndex)
+        val cell = gridWithEmptyCells[coord]
+
+        if (cell.isEmpty()) continue // already an empty cell.
+
+        gridWithEmptyCells.clearCell(coord)
+
+        val s = gridWithEmptyCells.copy()
+
+        val numOfSolutions = numberOfSolutions(s)
+        if (numOfSolutions == 1) {
+            cellsRemoved.add(cell)
+        } else {
+            gridWithEmptyCells[coord] = cell
+        }
+    }
+
+    return Pair(gridWithEmptyCells, cellsRemoved)
+}
+
+// TODO: write tests for this function.
+fun numberOfSolutions(grid: Grid, count: Int = 0, coord: Coord = Coord(0, 0)): Int {
+    var countVar = count
+    val nextCoord = grid.nextCoord(coord) ?: return (1 + countVar)
+
+    if (grid[coord].isNonEmpty()) return numberOfSolutions(grid, countVar, nextCoord)
+
+    for (value in 1..9) {
+        if (!grid.legal(coord, value)) continue
+
+        val newCell = Cell(coord = coord, value)
+        grid[newCell.coord] = newCell
+
+        countVar = numberOfSolutions(grid, countVar, nextCoord)
+        if (countVar > 1) return countVar
+    }
+    grid[coord] = Cell(coord = coord, value = 0)
+
+    return countVar
+}
